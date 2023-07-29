b9f9c0c1c5ea50b549cb7ae5f264caca5b747861
phumzani101 <phumzani101@gmail.com>
2023-07-29 18:14:01 +0200

    airbnb navbar

---
+++ src/components/ui/Avatar.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+
+const Avatar = () => {
+  let imgSrc = "/images/placeholder.jpg";
+  return (
+    <Image
+      src={imgSrc}
+      className="rounded-full hidden md:block "
+      height={30}
+      width={20}
+      alt="Avatar"
+    />
+  );
+};
+
+export default Avatar;
---
+++ src/components/ui/navbar/MenuItem.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+
+const MenuItem = ({
+  onClick,
+  label,
+}: {
+  onClick: () => void;
+  label: string;
+}) => {
+  return (
+    <div
+      onClick={onClick}
+      className="px-4 py-3 hover:bg-neutral-100 transition font-bold"
+    >
+      {label}
+    </div>
+  );
+};
+
+export default MenuItem;
